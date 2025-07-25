a1bc3fbfba8d7951450a607d18f2f26d265eff30
Jhonathan-Ferroni <ferroni.jhonathan2403@gmail.com>
2025-07-24 22:05:15 -0300

    Commit inicial: projeto ContaBancaria em C#

---
+++ DepositosPOO/ContaBancaria.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+
+namespace DepositosPOO
+{
+    internal class ContaBancaria
+    {
+        //encapsulamento
+        private string _numero;
+        public string Nome { get; set; }
+        public char Deposito { get; private set; }
+        public double SaldoInicial { get; private set; }
+
+        //construtor para deposito == 's'
+        public ContaBancaria(string numero, string nome, char deposito, double valor)
+        {
+            _numero = numero;
+            Nome = nome;
+            Deposito = deposito;
+            SaldoInicial = valor;
+        }
+
+        //construtor para deposito == 'n'
+        public ContaBancaria(string numero, string nome, char deposito)
+        {
+            _numero = numero;
+            Nome = nome;
+            Deposito = deposito;
+            SaldoInicial = 0;
+
+        }
+
+        //Propertie
+        public string Numero
+        {
+            get { return _numero; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _numero = value;
+                }
+            }
+        }
+
+        //funções
+
+        public void AdicionarValor(double valor)
+        {
+            SaldoInicial += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            SaldoInicial -= valor + 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "+ _numero + ", Titular: " + Nome + ", Saldo: $ " + SaldoInicial;
+        }
+    }
+}
---
+++ DepositosPOO/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace DepositosPOO
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Entre o número da conta: ");
+            string conta = Console.ReadLine();
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char opc = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            ContaBancaria c;
+
+            if (opc == 's')
+            {
+                Console.Write("Entre o valor do depósito inicial: ");
+                double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new ContaBancaria(conta, titular, opc, deposito);
+                Console.WriteLine("");
+            }
+            else if(opc == 'n')
+            {
+                c = new ContaBancaria(conta, titular, opc);
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("Digite uma opção válida");
+                return;
+            }
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c.ToString());
+            Console.WriteLine("");
+
+            Console.Write("Entre um valor para depósito: ");
+            double segundodeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            c.AdicionarValor(segundodeposito);
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c.ToString());
+            Console.WriteLine("");
+
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            c.Saque(saque);
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c.ToString());
+        }
+    }
+}
